9e0201ac2e3798a3a8d63b916021fc9ce43af956
John McLear <john@mclear.co.uk>
2015-12-21 17:40:32 +0000

    example of HTTP call for CSS

--- export.js
+++ export.js
@@ -1,10 +1,12 @@
 var pdf = require("./node_modules/html-pdf");
 var fs = require("fs");
 var settings = require("ep_etherpad-lite/node/utils/Settings");
+var request = require('request');
 
 exports.exportConvert = function(hook_name, args, callback){
   var srcFile = args.srcFile;
   var destFile = args.destFile;
+  var req = args.req;
 
   if(!settings.ep_pdf){
     settings.ep_pdf = {};
@@ -40,15 +42,35 @@ exports.exportConvert = function(hook_name, args, callback){
 
   var html = fs.readFileSync(srcFile, 'utf8')
 
-  pdf.create(html, options).toFile(destFile, function(err, res){
-    // console.log(res.filename);
-    if(err){
-      console.error("error during PDF handling");
-      return callback(err);
-    }else{
-      console.log("Success rendering PDF using plugin");
-      return callback(null, "success");
-    }
-  });
+  // Load the formatting from an endpoint
+  if(req.query && req.query.format && req.query.format === "formal"){
+    // Formal endpoint
+    console.log("loading css from gist");
+    request("https://gist.githubusercontent.com/JohnMcLear/063a3b21888447f6a76c/raw/d311b02601994469915a6aa04dca364e69ec1f42/gistfile1.txt",
+    function(e,r,css){
+      html = html.replace("</style>", css + "</style>");
+      pdf.create(html, options).toFile(destFile, function(err, res){
+        // console.log(res.filename);
+        if(err){
+          console.error("error during PDF handling");
+          return callback(err);
+        }else{
+          console.log("Success rendering PDF using plugin");
+          return callback(null, "success");
+        }
+      });
+    });
+  }else{
+    pdf.create(html, options).toFile(destFile, function(err, res){
+      // console.log(res.filename);
+      if(err){
+        console.error("error during PDF handling");
+        return callback(err);
+      }else{
+        console.log("Success rendering PDF using plugin");
+        return callback(null, "success");
+      }
+    });
+  }
 }
 
